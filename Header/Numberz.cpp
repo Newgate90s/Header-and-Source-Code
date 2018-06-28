9c4b0550c1c29eb38eccbb1f14998f5ce0dbe0e0
SAMAEL NEWGATE <SAMNEWGA@uat.edu>
2018-06-27 21:14:40 -0700

    Add project files.

---
+++ Header/Numberz.cpp
@@ -0,0 +1,32 @@
+#include"stdafx.h"
+#include "Numberz.h"
+
+
+//Addition
+double NumberAdd(double A, double B) {
+
+	//How the sum is found
+	double sum = A + B;
+
+	return sum;
+}
+
+//Division
+double NumberDivide(double a, double b) {
+
+	//How the quotient is found
+	double quotient = a / b;
+
+	return quotient;
+
+}
+
+
+//Multiplication
+double NumberMultiply(double a, double b) {
+
+	//How the product is found
+	double product = a * b;
+
+	return product;
+}
---
+++ Header/Numberz.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "stdafx.h"
+
+//Adding numbers function
+double NumberAdd(double a, double b);
+
+//Multiplying numbers function
+double NumberMultiply(double a, double b);
+
+//Dividing numbers function
+double NumberDivide(double a, double b);
+
+
